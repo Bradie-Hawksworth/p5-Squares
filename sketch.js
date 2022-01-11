89002656462ebf13976ea9370fc1aaad6eb460d2
Bradie-Hawksworth <67816866+Bradie-Hawksworth@users.noreply.github.com>
2022-01-11 23:01:11 +0000

    Add files via upload
    Added p5 files for square exercise

---
+++ sketch.js
@@ -0,0 +1,16 @@
+//The setup() function is called once when the program starts.
+//https://p5js.org/reference/#/p5/setup
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+//Called directly after setup(), the draw() function continuously executes the lines of code contained inside its block until the program is stopped or noLoop() is called.
+//https://p5js.org/reference/#/p5/draw
+function draw() {
+  background(220);
+  
+  //Create a square at x:10 y:20 size:50
+  // https://p5js.org/reference/#/p5/square
+  square(random(100),random(100),50);
+}
